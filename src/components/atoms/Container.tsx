c319ebec83b908c127b87b8cf5aa0225920709d3
TakumaIkeda <i.taku0415@gmail.com>
2022-08-24 06:16:05 +0900

    fix container component

--- src/components/atoms/Container.tsx
+++ src/components/atoms/Container.tsx
@@ -3,7 +3,11 @@ type Props = {
 };
 
 const Container = ({ children }: Props): JSX.Element => {
-  return <div className="md:w-9/12 p-2 min-h-screen">{children}</div>;
+  return (
+    <div className="flex justify-center p-2 min-h-screen">
+      <div className="md:w-9/12">{children}</div>
+    </div>
+  );
 };
 
 export default Container;
